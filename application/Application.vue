b7eec1f0820879e63e587298a3cf19bd04301588
Vovikkko <45092587+Beenlord@users.noreply.github.com>
2023-01-10 23:02:20 +0300

    Initial

---
+++ application/Application.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="Application"></div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    },
+};
+</script>
+
+<style lang="scss">
+.Application {
+}
+</style>
---
+++ index.js
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import Application from "./application/Application.vue";
+
+const app = createApp(Application);
+app.mount("#application");

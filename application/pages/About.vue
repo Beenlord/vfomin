007a93f70c9aaac7c9ae441b8520f07a91b4a88f
Beenlord <tombeenlord@gmail.com>
2025-01-02 01:57:31 +0300

    Wip

--- application/pages/About.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div class="AbputPAge">
-        <h1>Станица <b>/about</b></h1>
-        <a href="/">Назад</a>
-    </div>
-</template>
--- application/pages/Home.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div class="HomePAge">
-        <h1>Станица <b>/home</b></h1>
-        <a href="/about">Страница about</a>
-    </div>
-</template>

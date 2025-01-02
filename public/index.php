222b5801a271fffe674c2aad8b4c599ab2baafd0
Beenlord <tombeenlord@gmail.com>
2025-01-02 03:16:51 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,2 +1,12 @@
-<?php
-    echo "Welcome to: vfomin.com (test 2)";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>🍃 Vovik</title>
+</head>
+<body>
+    <p>Welcome to</p>
+    <h1>Vovik's site!</h1>
+</body>
+</html>

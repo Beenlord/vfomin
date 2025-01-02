8c9ca8811b017ec84989016c931c9a4279f369e5
Beenlord <tombeenlord@gmail.com>
2025-01-02 04:05:13 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,10 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Vladimir Aleksandrovich</title>
+
+    <style>
+        * {
+            box-sizing: border-box;
+            padding: 0;
+            margin: 0;
+        }
+        html,
+        body {
+            width: 100vw;
+            height: 100vh;
+            height: 100dvh;
+            max-width: 1200px;
+            margin: 0 auto;
+        }
+    </style>
 </head>
 <body>
-    <p>Welcome to</p>
-    <h1>Vovik's site!</h1>
+    <h1>
+        Vladimir Aleksandrovich
+    </h1>
+    <p>
+        Personal source
+    </p>
 </body>
 </html>
